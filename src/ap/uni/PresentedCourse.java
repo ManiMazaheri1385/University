edd9c632fd49a67c64a752ef5735cdd1ef56f3fd
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-02-27 05:08:58 +0330

    add presented course class

--- src/ap/uni/PresentedCourse.java
+++ src/ap/uni/PresentedCourse.java
@@ -1,7 +1,40 @@
 package ap.uni;
 
+import java.util.ArrayList;
+
 public class PresentedCourse {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static ArrayList<PresentedCourse> presentedCourseList = new ArrayList<PresentedCourse>();
+    public int presentedCourseID;
+    public int courseID;
+    public int professorID;
+    public int maxCapacity;
+    public static ArrayList<Integer> studentIDList;
+
+    public PresentedCourse(int courseID, int professorID, int maxCapacity) {
+        this.courseID = courseID;
+        this.professorID = professorID;
+        this.maxCapacity = maxCapacity;
+        studentIDList = new ArrayList<Integer>(maxCapacity);
+        presentedCourseList.add(this);
+        presentedCourseID = presentedCourseList.size();
     }
+
+    public static PresentedCourse findById(int ID) {
+        for (PresentedCourse presentedCourse : presentedCourseList) {
+            if (ID == presentedCourse.presentedCourseID) {
+                return presentedCourse;
+            }
+        }
+        return null;
+    }
+
+    public void addStudent(int studentID) {
+        if (studentIDList.size() < maxCapacity) {
+            studentIDList.add(studentID);
+        } else {
+            System.out.println("student list is full");
+        }
+    }
+
 }
