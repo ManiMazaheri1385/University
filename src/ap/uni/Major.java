d93f85a1c7e6bb34e58c194cc9323186fb55a213
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-02-27 05:01:53 +0330

    add major class

--- src/ap/uni/Major.java
+++ src/ap/uni/Major.java
@@ -1,7 +1,38 @@
 package ap.uni;
 
+import java.util.ArrayList;
+
 public class Major {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static ArrayList<Major> majorList = new ArrayList<Major>();
+    public static int numberOfStudents;
+    public int majorID;
+    public String name;
+    public final int maxCapacity;
+
+    public Major(String name, int maxStudentNumber) {
+        this.name = name;
+        maxCapacity = maxStudentNumber;
+        majorList.add(this);
+        majorID = majorList.size();
     }
+
+    public static Major findById(int ID) {
+        for (Major major : majorList) {
+            if (ID == major.majorID) {
+                return major;
+            }
+        }
+        return null;
+    }
+
+    public void addStudent() {
+        if (numberOfStudents < maxCapacity) {
+            numberOfStudents++;
+        }
+        else {
+            System.out.println("student is over max capacity");
+        }
+    }
+
 }
