b8c57e681de62ea3e8b7ef4025447ad99cb0eef7
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-02-27 04:59:17 +0330

    add person class

--- src/ap/base/Person.java
+++ src/ap/base/Person.java
@@ -1,7 +1,28 @@
 package ap.base;
 
+import java.util.*;
+
 public class Person {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public int personID;
+    public String name;
+    public final String nationalID;
+    public static ArrayList<Person> personList = new ArrayList<Person>();
+
+    public Person(String name, String nationalID) {
+        this.name = name;
+        this.nationalID = nationalID;
+        personList.add(this);
+        personID = personList.size();
     }
+
+    public static Person findById(int ID) {
+        for (Person person : personList) {
+            if (ID == person.personID) {
+                return person;
+            }
+        }
+        return null;
+    }
+
 }
