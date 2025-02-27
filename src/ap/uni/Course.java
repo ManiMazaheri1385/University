bb4afeae560b083b8248fda96ba420c215a7a344
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-02-27 05:06:43 +0330

    add course class

--- src/ap/uni/Course.java
+++ src/ap/uni/Course.java
@@ -1,7 +1,27 @@
 package ap.uni;
 
+import java.util.ArrayList;
+
 public class Course {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public int courseID;
+    public String title;
+    public int units;
+    public static ArrayList<Course> courseList = new ArrayList<Course>();
+
+    public Course(String title, int units) {
+        this.title = title;
+        this.units = units;
+        courseList.add(this);
+        courseID = courseList.size();
+    }
+
+    public static Course findById(int ID) {
+        for (Course course : courseList) {
+            if (ID == course.courseID) {
+                return course;
+            }
+        }
+        return null;
     }
 }
