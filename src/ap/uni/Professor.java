67e21d9c44632414f28a6d6e5aeba3ba7b657e88
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-02-27 05:05:06 +0330

    add professor class

--- src/ap/uni/Professor.java
+++ src/ap/uni/Professor.java
@@ -1,7 +1,28 @@
 package ap.uni;
 
+import java.util.ArrayList;
+
 public class Professor {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public int professorID;
+    public int personID;
+    public int majorID;
+    public static ArrayList<Professor> professorList = new ArrayList<Professor>();
+
+    public Professor(int personID, int majorID) {
+        this.personID = personID;
+        this.majorID = majorID;
+        professorList.add(this);
+        professorID = professorList.size();
     }
+
+    public static Professor findById(int ID) {
+        for (Professor professor : professorList) {
+            if (ID == professor.professorID) {
+                return professor;
+            }
+        }
+        return null;
+    }
+
 }
